5c9b108f2b33e2f6d1b07d70635b04ecfcf25785
ezequielfilipesantos <a21192@alunos.ipca.pt>
2024-01-06 22:29:09 +0000

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -5,7 +5,7 @@ module.exports = function(req, res, next) {
         return next(); // Allow access to the route
     } else {
         // Skip redirection for index and login routes
-        if (req.originalUrl === '/' || req.originalUrl === '/login') {
+        if (req.originalUrl === '/' || req.originalUrl === '/loginPage') {
             return next();
         }
         res.redirect('/loginPage'); // Redirect to login if not authenticated
