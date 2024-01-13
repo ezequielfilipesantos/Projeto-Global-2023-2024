fe3768e90cd5f943bd1f6bfd19f7866089569092
ezequielfilipesantos <a21192@alunos.ipca.pt>
2024-01-13 19:21:59 +0000

    Update 1

--- routes/requestsHistoryRouter.js
+++ routes/requestsHistoryRouter.js
@@ -1,24 +1,29 @@
 const express = require('express');
 const router = express.Router();
 
-// Define a route for displaying Pedido Avaliação Médica history
-router.get('/requestsHistory', async (req, res) => {
-  try {
-    const utenteID = req.session.userID; // Get the UtenteID from the session
+// Export a function that takes the pool object as an argument
+module.exports = function(pool) {
+    const router = express.Router();
 
-    const queryText = `
-      SELECT *
-      FROM PedidoAvaliaçãoMédica
-      WHERE UtenteUtenteID = $1
-      ORDER BY datapedido DESC;
-    `;
+    // Define a route for displaying Pedido Avaliação Médica history
+    router.get('/', async (req, res) => {
+        try {
+            const utenteID = req.session.userID; // Get the UtenteID from the session
 
-    const { rows } = await pool.query(queryText, [utenteID]);
-    res.render('views/autenticated_utente/requestsHistory', { pedidoAvaliacaoMedicaRecords: rows });
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Internal Server Error');
-  }
-});
+            const queryText = `
+                SELECT *
+                FROM PedidoAvaliaçãoMédica
+                WHERE UtenteUtenteID = $1
+                ORDER BY datapedido DESC;
+            `;
 
-module.exports = router;
+            const { rows } = await pool.query(queryText, [utenteID]);
+            res.render('autenticated_utente/requestsHistory', { pedidoAvaliacaoMedicaRecords: rows });
+        } catch (error) {
+            console.error(error);
+            res.status(500).send('Internal Server Error');
+        }
+    });
+
+    return router;
+};
